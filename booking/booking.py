1d8d7923c966633db97ff05d3fde2f72216e3972
tami1326 <andreea_tc26@yahoo.com>
2023-11-07 17:52:57 +0200

    commit

---
+++ booking/booking.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+import os
+import booking.constants as const
+
+class Booking(webdriver.Chrome):
+    def __init__(self, driver_path = r"\\drivers\\chromedriver-mac-x64\\chromedriver"):
+        self.driver_path = driver_path
+        os.environ['PATH'] += self.driver_path
+        super(Booking, self).__init__()
+
+    def land_first_page(self):
+        self.get(const.BASE_URL)
---
+++ run.py
@@ -0,0 +1,2 @@
+from booking.constants import a
+
